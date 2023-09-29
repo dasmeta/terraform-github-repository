a4e63cf662d14eb71a4d5d622ec59d704bc8c2b8
viktoryathegreat <ghazaryan.victorya@gmail.com>
2023-09-29 13:54:04 +0400

    feat(DMVP-2779): Added tf resource to manage github actions enabling

---
+++ actions.tf
@@ -0,0 +1,15 @@
+resource "github_actions_repository_permissions" "actions" {
+  allowed_actions = var.enable_github_actions
+
+  dynamic "allowed_actions_config" {
+    for_each = var.allowed_github_actions_config != null ? [1] : []
+
+    content {
+      github_owned_allowed = var.allowed_github_actions_config.github_owned_allowed
+      patterns_allowed     = var.allowed_github_actions_config.patterns_allowed
+      verified_allowed     = var.allowed_github_actions_config.verified_allowed
+    }
+  }
+
+  repository = github_repository.repository[0].name
+}
